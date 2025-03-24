f7dbcaf2117372fdb37a1546ba6f2e518e3079c2
Haume0 <haume341@outlook.com>
2025-03-24 14:55:47 +0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module axo
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+/*
+🪐 Welcome to Axo ✨
+AxoScaffold is a Restful API scaffold for Go, built on top of stdlib and gorm.
+It is designed to be simple, fast, and easy to use.
+For more information, please visit: https://haume.me/axo-scaffold
+
+Licanse: MIT
+Copyright (c) 2025 Haume
+It's not neccesary but i'll be greatful if you give me a star on GitHub and mention me in your project.
+*/
+
+func main() {
+	// 🔐 Getting the environment variables
+	// 🏁 Initializations
+	// 🏗️ Creating the router
+	// 🌐 Registering the routes
+	// ⚙️ Adding middlewares
+	// 💢 Adding cors setup
+	// 🚦 Starting the server
+}
